19f7659e3058c8291f29069bfd27020389fcb822
maekawa-kazuma <kazumaekawa7218@gmail.com>
2022-01-26 22:32:58 +0900

    app.vue編集

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,18 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <h1>Todo App</h1>
+    <p v-for="user in state.users" v-bind:key="user.id">
+      {{ user.nickname }}
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, onMounted } from 'vue' // defineComponent関数（TypeScriptを取り入れる場合に必要）などを読み込む
+import { getUsers } from './lib/api/users' // APIを呼び出すための関数を読み込む
+import { User } from './interfaces/index' // 型定義を読み込む
+
+interface State {
+  users: User[]
+  nickname: string
+}
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const state = reactive<State>({
+      users: [],
+      nickname: ''
+    })
+
+    // ユーザー一覧を取得
+    const handleGetUsers = async (): Promise<void> => {
+      try {
+        const res = await getUsers()
+
+        if (res?.status === 200) {
+          state.users = res.data.users // users（配列）に結果を格納
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    // Vueインスタンスがマウントされるたびに実行
+    onMounted(() => {
+      handleGetUsers()
+    })
+
+    return {
+      state
+    }
   }
-});
+})
 </script>
 
 <style>
